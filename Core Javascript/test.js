74a02f94df9f6ca501ccac156593045514039724
Vivek <86698149+Vivek0311@users.noreply.github.com>
2023-12-21 11:34:11 +0530

    Update test.js

--- Core Javascript/test.js
+++ Core Javascript/test.js
@@ -1,13 +1,13 @@
-// let initial = performance.now();
-// let a = 100;
-// while(a>0){
-//     console.log(a);
-//     a--;
-// }
+let initial = performance.now();
+let a = 100;
+while(a>0){
+    console.log(a);
+    a--;
+}
 
-// let final = performance.now();
+let final = performance.now();
 
-// console.log(final)
+console.log(final)
 
 
 
