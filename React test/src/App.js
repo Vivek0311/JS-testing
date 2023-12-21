909b1b8608fee5c3824c1b974ddb855015a6579c
Vivek <86698149+Vivek0311@users.noreply.github.com>
2023-12-21 11:11:20 +0530

    Add files via upload

---
+++ React test/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1> This is my 1st react app</h1>
+    <h1> I'm learning react and pushing in git hub</h1>
+    </div>
+  );
+}
+
+export default App;
